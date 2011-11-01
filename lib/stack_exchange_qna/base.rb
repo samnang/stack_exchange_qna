635b2a579647f71c54fb18b43887ab0fe1c60da9
Samnang Chhun <samnang.chhun@gmail.com>
2011-11-01 17:49:49 +0700

    Move files

--- lib/stack_exchange_qna/base.rb
+++ lib/stack_exchange_qna/base.rb
@@ -16,6 +16,22 @@ class StackExchangeQnA::Base < Hashie::Mash
       parse_response_collection(response).first
     end
 
+    def self.where(hash)
+      QueryMethods.new(self).where(hash)
+    end
+
+    def self.pagesize(number)
+      QueryMethods.new(self).pagesize(number)
+    end
+
+    def self.page(number)
+      QueryMethods.new(self).page(number)
+    end
+
+    def self.includes(*args)
+      QueryMethods.new(self).includes(*args)
+    end
+
     private
 
     def make_request(url)
--- spec/stack_exchange_qna/query_methods_spec.rb
+++ spec/stack_exchange_qna/query_methods_spec.rb
@@ -10,5 +10,13 @@ module StackExchangeQnA
 
       query.query_string.should eql("tagged=Ruby&page=2&pagesize=42&answers=true")
     end
+
+    it "should be able to get list objects with including its assocation" do
+      pending
+      questions = Question.where(:tagged => "Ruby").includes(:answers)
+
+
+      questions.first.answers.count.should > 0
+    end
   end
 end
